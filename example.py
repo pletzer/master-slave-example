ec71371fad445f581b40ebda34f05d40d61db75b
Alex Pletzer <alexander@gokliya.net>
2018-10-27 16:49:16 +1300

    runnning for all pes but not performing all the tasks if there are not enough pes

--- example.py
+++ example.py
@@ -1,34 +1,61 @@
 """
 """
 import numpy
+import time
 from mpi4py import MPI
 
-TODO, PENDING, DONE, INDEFINITE = 0, 1, 2, 3
+READY, BUSY, BYE = 1, 2, 3
+STATUS_LINE, WORK_LINE = 100, 101
 
 
-def task(xs):
-	return [x**2 for x in xs]
+def performTask(data):
+    time.sleep(2) # simulates a long task
+
 
 comm = MPI.COMM_WORLD
 pe = comm.Get_rank()
 nprocs = comm.Get_size()
 master = 0
+worker = 1
 
-ntasks = 10
-ndims = 3
-
-statusData = numpy.array([INDEFINITE]*ntasks, numpy.int)
-
-statusWindow = MPI.Win.Create(statusData, 1, MPI.INFO_NULL, comm=comm)
+ntasks = 3
 
 if pe == master:
-    statusData[:] = TODO
 
-statusWindow.Fence()
-statusWindow.Get(statusData, master)
-statusWindow.Fence()
+	# all the workers are ready
+	workerStatus = {worker: READY for worker in range(1, nprocs)}
 
-print('[{}] status = {}'.format(pe, statusData))
+	for taskId in range(ntasks):
 
-statusWindow.Free()
+		# find a worker who is ready to take on some new tasks
+		for worker, status in workerStatus.items():
+
+			if status == READY:
+
+				comm.send(taskId, dest=worker, tag=WORK_LINE)
+				workerStatus[worker] = BUSY
+				print('worker {} is now busy...'.format(worker))
+				break
+
+	# tell the workers to shutdown
+	for worker in range(1, nprocs):
+		comm.send(None, dest=worker, tag=WORK_LINE)
+
+
+else:
+
+	# workers
+
+	status = READY
+	while status != BYE:
+		# get order
+		data = comm.recv(source=master, tag=WORK_LINE)
+		if data is None:
+			status = BYE
+			print('[{}] is shutting down...'.format(pe))
+		else:
+			print('[{}] performs task {}'.format(pe, data))
+			performTask(data)
+			#comm.send(READY, dest=master, tag=STATUS_LINE)
+
 
